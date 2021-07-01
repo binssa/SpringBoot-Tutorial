90851fdef96e325ac57dded2fa3b2ce466ee103b
binssa <bin.sa0823@gmail.com>
2021-07-01 22:51:54 +0900

    Create Student Class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import com.sun.tools.javac.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 @SpringBootApplication
 @RestController
@@ -15,7 +19,15 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World");
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Ben",
+						"ben.sa@gmail.com",
+						LocalDate.of(2000, Month.AUGUST, 23),
+						22
+				)
+		);
 	}
 }
